2dae214d45f7522b308b77ef150b7c9862e02e8b
mqli <limanqing315@gmail.com>
2011-12-02 23:44:11 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@
 /*
  * GET home page.
  */
-var posts = require('../model/posts');
+
 exports.index = function(req, res){
-  posts.findAll(function(posts){
-    res.render('index', { title: 'BlueJukebox',posts: posts });
-  });
+  res.render('index', { title: 'BlueJukebox'});
 };
