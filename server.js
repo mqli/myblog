7f50261d71f5c5a7f961eb548c14311b3569c2cb
mqli <limanqing315@gmail.com>
2011-12-03 00:18:11 +0800

    update

--- server.js
+++ server.js
@@ -1,35 +1,4 @@
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http');
-
-var app = module.exports = express();
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-app.get('/', routes.index);
-
-//http.createServer(app).listen(process.env.PORT || 8001);
-
+var http = require('http');
  
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
@@ -37,4 +6,3 @@ var server = http.createServer(function (req, res) {
 });
  
 server.listen(process.env.PORT || 8001);
-console.log("Express server listening for connections");
