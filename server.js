3ad7afdd5e1208e31557c442e8c0201a8d17b06d
mqli <limanqing315@gmail.com>
2011-12-02 23:39:54 +0800

    update

--- server.js
+++ server.js
@@ -1,35 +1,6 @@
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http');
-
-var app = module.exports = express();
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-app.get('/', routes.index);
-
-//app.listen(process.env.PORT || 80);
+var http = require('http');
+ 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
   res.end("Hello world\n");
