d19c906bfcf69d1d93d6560b598f72273d4dd79e
Administrator <mqli@ubuntu.ubuntu-domain>
2012-09-22 23:01:51 +0800

    update

--- public/javascript/tools.js
+++ public/javascript/tools.js
@@ -1,27 +1,50 @@
 $(function () {
-  var prices = {}
+  var Hospital = {
+    _hospitals: {},
+    add: function (name, price) {
+      if (!this._hospitals[name]) {
+        $('select.hospital').append('<option>' + name + '</option>');
+        Bill.addHospital(name, price);
+      }
+      this._hospitals[name] = parseInt(price);
+      Bill.updatePrice(name, price);
+    },
+    getPirce: function (name) {
+      return this._hospitals[name];
+    }
+  },
+  Students = {
+    add: function (className, amount, hospital) {
+      Bill.addStudent(className, parseInt(amount), hospital);
+    }
+  },
+  Bill = {
+    addHospital: function (name, price) {
+      $('#output tbody').append('<tr id="'+name+'"><td>' + name + '</td><td>' + price + '</td><td>' + 0 + '</td><td>' + 0 + '</td><td>' + 0 + '</td><td>' + 0 + '</td></tr>' );
+    },
+    updatePrice: function (name, price) {
+      $('#' + name + ' td:eq(1)').html(price);
+    },
+    addStudent: function (className, amount, hospital) {
+      $('#' + hospital + ' td:eq(5)').html(function () {
+        return parseInt($(this).text()) + (Hospital.getPirce(hospital) * amount);
+      });
+    }
+  };
+  
   $('#addHospital').click(function () {
     var $li = $(this).prev(),
         name = $li.find('.name').val(),
         price = $li.find('.price').val();
     if (name && price) {
-      if (!prices[name]){
-        $('#output tbody').append('<tr id="'+name+'"><td>' + name + '</td><td>' + price + '</td><td>' + 0 + '</td></tr>' );
-        $('select.hospital').append('<option>' + name + '</option>');
-      } else {
-        $('#' + name + ' td:eq(1)').html(price);
-      }
-      prices[name] = parseInt(price);
+      Hospital.add(name, price);
     }
   });
   $('#addStudent').click(function () {
-    var count = $(this).prev().find('input').val(),
-        name = $(this).prev().find('option:selected').text();
-    if (!count) {
-      return;
-    }
-    $('#' + name + ' td:eq(2)').html(function () {
-      return parseInt($(this).text()) + (prices[name] * parseInt(count));
-    });
+    var amount = $(this).prev().find('input:eq(1)').val(),
+        hospital = $(this).prev().find('option:selected').text();
+    if (amount) {
+       Students.add(null,amount, hospital);
+    }  
   });
 });
