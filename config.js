407b017a669803f298cf406b552cd944d93824d8
Administrator <mqli@ubuntu.ubuntu-domain>
2012-09-22 18:49:49 +0800

    update

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-	SERVER_PORT: process.env.PORT || 8001,
+	SERVER_HOST: process.env.VCAP_APP_HOST || 'localhost',
+	SERVER_PORT: process.env.VCAP_APP_PORT || process.env.PORT || 8001,
 	MONGO_HOST: 'localhost',
 	MONGO_PORT: 27017
 }
