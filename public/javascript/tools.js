a550ffbcf9b24f6e228c1d905edbe689391d0743
mqli <limanqing315@gmail.com>
2012-09-24 17:22:02 +0800

    update

--- public/javascript/tools.js
+++ public/javascript/tools.js
@@ -1,51 +1,57 @@
 $(function () {
   var Hospital = {
     _hospitals: {},
-    add: function (name, price) {
-      if (!this._hospitals[name]) {
-        $('#students_hospital').append('<option>' + name + '</option>');
-        $('#hospitals').append('<tr><td>' + name +'</td><td class="'+name+'">'+price+'</td></tr>')
-        Bill.addHospital(name, price);
+    add: function (attr) {
+      if (!(attr.name && attr.price)) {
+        return;
       }
-      this._hospitals[name] = parseInt(price);
-      $('#hospitals .' + name).html(price);
-      Bill.updatePrice(name, price);
+      if (!this._hospitals[attr.name]) {
+        $('#students_hospital').append('<option>' + attr.name + '</option>');
+        $('#hospitals').append('<tr><td>' + attr.name +'</td><td class="'+attr.name+'">'+attr.price+'</td></tr>')
+        Bill.addHospital(attr.name, attr.price);
+      }
+      this._hospitals[attr.name] = parseInt(attr.price);
+      $('#hospitals .' + attr.name).html(attr.price);
+      Bill.updatePrice(attr.name, attr.price);
     },
     getPirce: function (name) {
       return this._hospitals[name];
     }
   },
   Students = {
-    add: function (className, amount, hospital) {
-      Bill.addStudent(className, parseInt(amount), hospital);
+    add: function (attr) {
+      if (!(attr.className && attr.amount && attr.time && attr.hospital)) {
+        return;
+      }
+      $('#students').append('<tr><td>' + attr.className +'</td><td class="'+attr.className+'">'+attr.amount+'</td><td>' + attr.time +'</td><td>' + attr.hospital +'</td></tr>');
+      Bill.addStudent(attr);
     }
   },
   Bill = {
     addHospital: function (name, price) {
-      $('#output tbody').append('<tr><td>' + name + '</td><td class="'+name+'">' + price + '</td><td>' + 0 + '</td><td>' + 0 + '</td><td>0</td><td>' + 0 + '</td><td class="'+name+'_total">' + 0 + '</td></tr>' );
+      $('#output tbody').append('<tr><td>' + name + '</td><td>--</td><td> -- </td><td>--</td><td> -- </td><td class="'+name+'_total">' + 0 + '</td></tr>' );
     },
     updatePrice: function (name, price) {
       $('#output .' + name).html(price);
     },
-    addStudent: function (className, amount, hospital) {
-      $('#output .' + hospital + '_total').html(function () {
-        return parseInt($(this).text()) + (Hospital.getPirce(hospital) * amount);
-      });
+    addStudent: function (attr) {
+      $('#output .' + attr.hospital + '_total').html(function () {
+        return parseInt($(this).text()) + (Hospital.getPirce(attr.hospital) *attr.time  * attr.amount);
+      }).parents('tr').before('<tr><td>' + attr.hospital + '</td><td>' + attr.className + '</td><td>' + attr.amount + '</td><td>' + attr.time + '</td><td>' + Hospital.getPirce(attr.hospital) * attr.time  * attr.amount + '</td><td>--</td><tr>');
     }
   };
-  
   $('#addHospital').click(function () {
-    var name = $('#hospital_name').val(),
-        price = $('#hospital_price').val();
-    if (name && price) {
-      Hospital.add(name, price);
-    }
+    Hospital.add({
+      name: $('#hospital_name').val(),
+      price : $('#hospital_price').val()
+    });
   });
   $('#addStudent').click(function () {
-    var amount = $('#students_count').val(),
-        hospital = $('#students_hospital option:selected').text();
-    if (amount) {
-       Students.add(null,amount, hospital);
-    }  
+    Students.add({
+      time: $('#time').val(),
+      className: $('#class_name').val(),
+      amount: $('#students_count').val(),
+      hospital: $('#students_hospital option:selected').text()
+    });
   });
 });
